5163b949e2a8d0652e075826cc874d1e39f8555a
Abhijeet <abhijeet@twigsoftwares.com>
2017-01-25 14:45:22 +0530

    pacagename

--- app/src/main/java/com/abhijeet/appupdatemanager/receivers/PeriodicTaskReceiver.java
+++ app/src/main/java/com/abhijeet/appupdatemanager/receivers/PeriodicTaskReceiver.java
@@ -2,10 +2,10 @@ package com.abhijeet.appupdatemanager.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 
 import com.abhijeet.appupdatemanager.MainActivity;
-
 /**
  * Created by Admin on 1/2/2017.
  */
@@ -23,7 +23,7 @@ public class PeriodicTaskReceiver extends BroadcastReceiver {
                 Intent i = new Intent(context.getApplicationContext(), MainActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(i);
-               // Toast.makeText(context.getApplicationContext(), "App installed " + intent.getData().toString(), Toast.LENGTH_LONG).show();
+                // Toast.makeText(context.getApplicationContext(), "App installed " + intent.getData().toString(), Toast.LENGTH_LONG).show();
             }
             if (intent.getAction().equals(Intent.ACTION_PACKAGE_REPLACED)) {
                 Intent i = new Intent(context.getApplicationContext(), MainActivity.class);
@@ -32,6 +32,16 @@ public class PeriodicTaskReceiver extends BroadcastReceiver {
                 //Toast.makeText(context.getApplicationContext(), "App replaced " + intent.getData().toString(), Toast.LENGTH_LONG).show();
             }
         }
+    }
+    public String getAppLable(Context context, String packageName) {
+        PackageManager packageManager = context.getPackageManager();
+        ApplicationInfo applicationInfo = null;
+        String thispackagename = context.getApplicationInfo().packageName;
+        try {
+            applicationInfo = packageManager.getApplicationInfo(packageName, 0);
+        } catch (final PackageManager.NameNotFoundException e) {
+        }
+        return (String) (applicationInfo != null ? packageManager.getApplicationLabel(applicationInfo) : "Unknown");
     }
 }
 
