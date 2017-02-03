4a833b84ac1276ae2420cbb67589d56ea4efb03b
Abhijeet <abhijeet@twigsoftwares.com>
2017-02-03 18:39:41 +0530

    added test for click

--- app/src/androidTest/java/com/abhijeet/appupdatemanager/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/abhijeet/appupdatemanager/ExampleInstrumentedTest.java
@@ -1,11 +1,16 @@
 package com.abhijeet.appupdatemanager;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static org.junit.Assert.*;
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -14,10 +19,33 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+//    @Test
+//    public void useAppContext() throws Exception {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//        assertEquals("com.abhijeet.appupdatemanager", appContext.getPackageName());
+//
+//
+//
+//
+//
+//    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
     @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-        assertEquals("com.abhijeet.appupdatemanager", appContext.getPackageName());
+    public void changeText_sameActivity() {
+
+
+        onView(withId(R.id.btnAddInstalledApps)).perform(click());
+
+        onView(withId(R.id.savebutton)).perform(click());
+
     }
+
+
+
+
 }
